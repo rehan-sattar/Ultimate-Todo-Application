a3d63477dd048bb350ec2a144d776bd5e79ceffb
Rehan-Sattar <rehansattar117@gmail.com>
2018-09-14 03:20:47 -0700

    Entry point to Application

---
+++ todo-app-part-3/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import AppContainer from "./components/AppContainer";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>The ultimate Todo list project partIII</h1>
+        <AppContainer />
+      </div>
+    );
+  }
+}
+
+export default App;
