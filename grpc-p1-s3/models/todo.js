de5011279db16c9fda59632d39f0d719382534a2
korai <zaynulabdin313@gmail.com>
2018-09-15 18:44:16 +0500

    initial commit grpc

---
+++ grpc-p1-s3/models/todo.js
@@ -0,0 +1,23 @@
+import * as mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+export const todo =new Schema({
+    Title:{
+        type:String,
+        required:"Enter title"
+    },
+    Description:{
+        type:String,
+        required:"Enter Description"
+    },
+    Done:{
+        type:Boolean,
+        default:false
+    },
+    CreatedAt:{
+        type:Date,
+        default:new Date()
+    }
+})
+
+module.exports = Mongoose.model('todo', todo);
