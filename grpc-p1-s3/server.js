e277dbb0db607da8c9f766a25c385a7d1f34bd0c
korai <zaynulabdin313@gmail.com>
2018-09-16 18:58:52 +0500

    gRPC cerver client working without database

--- grpc-p1-s3/server.js
+++ grpc-p1-s3/server.js
@@ -1,53 +1,61 @@
+
 const grpc = require('grpc');
-const proto = grpc.load('todo.proto');
+const protoTodos = grpc.load('todo.proto');
 const server = new grpc.Server();
 
-global.Mongoose = require('mongoose');
-var mongoDB = 'mongodb://localhost/grpctodo';
-Mongoose.connect(mongoDB, { useNewUrlParser: true });
-var db = Mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// global.Mongoose = require('mongoose');
+// var mongoDB = 'mongodb://localhost/grpctodo';
+// Mongoose.Promise = global.Promise;
+// Mongoose.connect(mongoDB, { useNewUrlParser: true })
+//     .then(() => console.log('Mongodb connection succesful'))
+//     .catch((err) => console.error(err));
+// const TodoDb = require('./tododb');
 
-const todoServices = require('./tododb');
+var todos = [{
+    id: 12,
+    title: 'Ultimate Todo App',
+    description: 'Ultimate Todo App Projects assigned by Sir Zia Khan and Team'
+}];
 
-server.addService(proto.todos.TodoService.service, {
+server.addService(protoTodos.todoproto.TodoService.service, {
 
-    list(call, callback) {
-        todoServices.list(callback);
+    list: function(_, callback) {
+        callback(null, todos);
     },
 
-    get(call, callback) {
-        let payload = {
-            criteria: {
-                id: call.request.id
-            },
-            projections: {
-                _id: 0
-            }
-        };
-        let emp = new todoServices(payload);
-        emp.fetch(callback);
-    },
-
-    add(call, callback) {
-        var todo = new todoServices({
-            id: call.request.id,
-            title: call.request.title,
-            description: call.request.description,
+    get: function(call, callback) {
+        for (var i = 0; i < todos.length; i++)
+            if (todos[i].id == call.request.id)
+                return callback(null, todos[i]);
+        callback({
+            code: grpc.status.NOT_FOUND,
+            details: 'Not found'
         });
-        todo.add(callback);
     },
 
-    delete(call, callback) {
-        const e = {
-            id: call.request.id,
-        };
-        var todo = new todoServices(e);
-        todo.remove(e, callback);
+
+    insert: function(call, callback) {
+        var todo = call.request;
+        todos.push(todo);
+        callback(null, {});
+    },
+
+    delete: function(call, callback) {
+        for (var i = 0; i < todos.length; i++) {
+            if (todos[i].id == call.request.id) {
+                todos.splice(i, 1);
+                return callback(null, {});
+            }
+        }
+        callback({
+            code: grpc.status.NOT_FOUND,
+            details: 'Not found'
+        });
     },
 });
 
+// gRPC Server
 server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
-
+console.log('grpc server starting on :', '0.0.0.0:50051');
 server.start();
-console.log('grpc server running on port:', '0.0.0.0:50051');
+console.log('grpc server running on :', '0.0.0.0:50051');
