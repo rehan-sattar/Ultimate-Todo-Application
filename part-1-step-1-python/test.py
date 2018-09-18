fbd86044302aa659c98ce3eb022e0383a47916c5
osamaimran25 <osamaimran25@gmail.com>
2018-09-18 14:53:46 -0700

    server more test added

--- part-1-step-1-python/test.py
+++ part-1-step-1-python/test.py
@@ -12,10 +12,17 @@ curl -i http://127.0.0.1:5000/api/v.1.0/1
 curl -i http://127.0.0.1:5000/api/v.1.0/5
 
 #add new task
-curl -X POST -H "Content-Type: application/json" -d   '{"id":3, "name ": "testing","done ": "yes","priority ": "high ","desc":"new task "}' http://127.0.0.1:5000/api/v.1.0
+curl -X POST -H "Content-Type: application/json" -d   "{""id"":4, ""name"": ""testing"",""done"": ""yes"",""priority"": ""high"",""desc"":""new task""}" http://127.0.0.1:5000/api/v.1.0
 
-#
-  
+# err: edit non-existing item
+curl -i -H "Content-Type: application/json" -X PUT -d '{\"name \": \"testing\",\"done\ ": \"yes\",\"priority\ ": \"high\ ",\"desc\":\"new task\ "}' http://127.0.0.1:5000/api/v.1.0/5
+
+ # edit existing item
+curl -i -H "Content-Type: application/json" -X PUT -d "{""name"": ""testing"",""done"": ""yes"",""priority"": ""high"",""desc"":""new task""}" http://127.0.0.1:5000/api/v.1.0/1
+ 
+#Deleting Task
+curl -i -H "Content-Type: application/json" -X DELETE http://127.0.0.1:5000/api/v.1.0/2
+ 
 
 
 
