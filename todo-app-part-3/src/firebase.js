2c0a3d4c82503636f2391a58ebd70f432abf071d
Adnan Hussain <adnanrajput42@gmail.com>
2018-09-20 17:56:34 +0500

    pwa implemented

--- todo-app-part-3/src/firebase.js
+++ todo-app-part-3/src/firebase.js
@@ -1,18 +1,31 @@
 import * as firebase from "firebase";
 const config = {
-    apiKey: "AIzaSyB95qumETKn5oHkm_iCpUV5EGbdcJeQODQ",
-    authDomain: "ultimate-todo-list-partiii.firebaseapp.com",
-    databaseURL: "https://ultimate-todo-list-partiii.firebaseio.com",
-    projectId: "ultimate-todo-list-partiii",
-    storageBucket: "ultimate-todo-list-partiii.appspot.com",
-    messagingSenderId: "241672973104"
+  apiKey: "AIzaSyB95qumETKn5oHkm_iCpUV5EGbdcJeQODQ",
+  authDomain: "ultimate-todo-list-partiii.firebaseapp.com",
+  databaseURL: "https://ultimate-todo-list-partiii.firebaseio.com",
+  projectId: "ultimate-todo-list-partiii",
+  storageBucket: "ultimate-todo-list-partiii.appspot.com",
+  messagingSenderId: "241672973104"
 };
 firebase.initializeApp(config);
 
 const fireStore = firebase.firestore();
 fireStore.settings({
-    timestampsInSnapshots: true
+  timestampsInSnapshots: true
 });
-export {
-    fireStore
-};
+
+fireStore
+  .enablePersistence()
+  .catch(function(err) {
+    if (err.code == "failed-precondition") {
+      // Multiple tabs open, persistence can only be enabled
+      // in one tab at a a time.
+      // ...
+    } else if (err.code == "unimplemented") {
+      // The current browser does not support all of the
+      // features required to enable persistence
+      // ...
+    }
+  });
+
+export { fireStore };
--- todo-app-part-3/src/index.js
+++ todo-app-part-3/src/index.js
@@ -4,11 +4,9 @@ import { Provider } from "react-redux";
 import './index.css';
 import App from './App';
 import store from "./store/index";
-import registerServiceWorker from './registerServiceWorker';
 import "./firebase";
 ReactDOM.render(
     <Provider store={store}>
         <App />
     </Provider>
     , document.getElementById('root'));
-registerServiceWorker();
