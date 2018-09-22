327c0ae101cfd59983b2e9430c329b50429751bd
korai <zaynulabdin313@gmail.com>
2018-09-22 16:15:56 +0500

    Containerization initial commit + erros resolved

--- grpc-p1-s3/sql/src/router/api.js
+++ grpc-p1-s3/sql/src/router/api.js
@@ -2,10 +2,10 @@ var express = require('express')
 var router = express.Router()
 var grpcClient = require('../grpcClient')
 
-router.get('/todos', grpcClient.todosList)
-router.post('/todos/add', grpcClient.insertTodo)
-router.get('/todos/:id', grpcClient.getTodo)
-router.put('/todos/edit/:id', grpcClient.updateTodo)
-router.delete('/todos/delete/:id', grpcClient.deleteTodo)
+router.get('/tasks', grpcClient.todosList)
+router.post('/tasks/add', grpcClient.insertTodo)
+router.get('/tasks/:id', grpcClient.getTodo)
+router.put('/tasks/edit/:id', grpcClient.updateTodo)
+router.delete('/tasks/delete/:id', grpcClient.deleteTodo)
 
 module.exports = router   
