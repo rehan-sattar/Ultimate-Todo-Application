246ef9c5c7093696d9919c0f1e5e33cf2b279aac
Rehan-Sattar <rehansattar117@gmail.com>
2018-09-22 09:45:00 -0700

    Redux Store

---
+++ part-2-step-4-grpc-nosql/src/store/Reducer.js
@@ -0,0 +1,52 @@
+import swal from "sweetalert";
+import { Actions } from './Actions';
+const defaultState = {
+    allTodos: null,
+    error: '',
+    addedTodo : false
+}
+const ultimateTodoReducer = (state = defaultState, action) => {
+    let updatedState = { ...state };
+    switch (action.type) {
+        // add task
+        case Actions.addTodoSuccess:
+            swal('Todo Added');
+            updatedState.addedTodo = true;
+            break;
+        case Actions.addTodoError:
+            swal(`Error: ${action.err} !`);
+            break;
+        case Actions.readAllTodoSuccess:
+            console.log("All todos from ",action.payload)
+            updatedState.allTodos = action.payload
+            break;
+        case Actions.readAllTodoError:
+            swal(`Error: ${action.err} !`);
+            break;
+        case Actions.deleteTodoSuccess:
+            swal('Task deleted!');
+            break;
+        case Actions.deleteTodoError:
+            swal(`Error: ${action.err} !`);
+            break;
+        case Actions.updateTodoSuccess:
+            console.log(action.type)
+            swal('Todo Updated!');
+            break;
+        case Actions.updateTodoError:
+            swal(`Error: ${action.err} !`);
+            break;
+        case Actions.taskDoneSuccess:
+            swal(`Task Done!`);
+            break;
+        case Actions.taskDoneError:
+            swal(`Error: ${action.err} !`);
+            break
+        default:
+            return updatedState;
+    }
+
+    return updatedState;
+};
+
+export default ultimateTodoReducer;
