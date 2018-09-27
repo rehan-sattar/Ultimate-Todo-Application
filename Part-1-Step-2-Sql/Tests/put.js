24b74da3c1962f30dced8fe4fd20a459fa4f7b79
Adnan Hussain <adnanrajput42@gmail.com>
2018-09-28 01:57:11 +0500

    put tests updated

--- Part-1-Step-2-Sql/Tests/put.js
+++ Part-1-Step-2-Sql/Tests/put.js
@@ -42,6 +42,28 @@ describe("Api Put test 2", () => {
 });
 
 describe("Api Put test  3", () => {
+  it("should return a response array of object containing updated todo", done => {
+    chai
+      .request("https://ultimate-todo-web-postgres.herokuapp.com")
+      //THE ID HERE MAY CHANGE IN FUTURE
+      .put("/todo/api/v1.0/todos/82e0bb0a-a9d0-4f8f-abd7-b18ed5abd51d")
+      .send({
+        title: "Title for test",
+        description: "New description"
+      })
+      .end((err, res) => {
+        res.body[0].should.have
+          .property("id")
+          .eql("82e0bb0a-a9d0-4f8f-abd7-b18ed5abd51d");
+        res.body[0].should.have.property("title").eql("Title for test");
+        res.body[0].should.have.property("description").eql("New description");
+        res.body[0].should.have.property("done");
+        done();
+      });
+  });
+});
+
+describe("Api Put test  4", () => {
   it("should return a response array of object containing message property", done => {
     chai
       .request("https://ultimate-todo-web-postgres.herokuapp.com")
@@ -58,7 +80,7 @@ describe("Api Put test  3", () => {
   });
 });
 
-describe("Api Put test  4", () => {
+describe("Api Put test  5", () => {
   it("should return a response array of object containing message property with success message", done => {
     chai
       .request("https://ultimate-todo-web-postgres.herokuapp.com")
@@ -77,7 +99,7 @@ describe("Api Put test  4", () => {
   });
 });
 
-describe("Api Put test  5", () => {
+describe("Api Put test  6", () => {
   it("should return a response array of object containing status property", done => {
     chai
       .request("https://ultimate-todo-web-postgres.herokuapp.com")
@@ -94,7 +116,7 @@ describe("Api Put test  5", () => {
   });
 });
 
-describe("Api Put test  6", () => {
+describe("Api Put test  7", () => {
   it("should return a response array of object containing status property with value true", done => {
     chai
       .request("https://ultimate-todo-web-postgres.herokuapp.com")
@@ -111,7 +133,7 @@ describe("Api Put test  6", () => {
   });
 });
 
-describe("Api Put test  7", () => {
+describe("Api Put test  8", () => {
   it("should returns internal server error since description is missing", done => {
     chai
       .request("https://ultimate-todo-web-postgres.herokuapp.com")
@@ -127,7 +149,7 @@ describe("Api Put test  7", () => {
   });
 });
 
-describe("Api Put test  8", () => {
+describe("Api Put test  9", () => {
   it("should returns a object property status", done => {
     chai
       .request("https://ultimate-todo-web-postgres.herokuapp.com")
@@ -137,13 +159,13 @@ describe("Api Put test  8", () => {
         title: "Title for test"
       })
       .end((err, res) => {
-        res.body[0].should.have.property("status");
+        res.body[res.body.length - 1].should.have.property("status");
         done();
       });
   });
 });
 
-describe("Api Put test  9", () => {
+describe("Api Put test  10", () => {
   it("should returns a object property status false", done => {
     chai
       .request("https://ultimate-todo-web-postgres.herokuapp.com")
@@ -153,14 +175,13 @@ describe("Api Put test  9", () => {
         title: "Title for test"
       })
       .end((err, res) => {
-        res.body[0].should.have.property("status")
-        .eql(false)
+        res.body[res.body.length - 1].should.have.property("status").eql(false);
         done();
       });
   });
 });
 
-describe("Api Put test  10", () => {
+describe("Api Put test  11", () => {
   it("should returns a object property message", done => {
     chai
       .request("https://ultimate-todo-web-postgres.herokuapp.com")
@@ -170,13 +191,12 @@ describe("Api Put test  10", () => {
         title: "Title for test"
       })
       .end((err, res) => {
-        res.body[0].should.have.property("message")
+        res.body[res.body.length - 1].should.have.property("message");
         done();
       });
   });
 });
-
-describe("Api Put test  11", () => {
+describe("Api Put test  12", () => {
   it("should returns a object property message with value", done => {
     chai
       .request("https://ultimate-todo-web-postgres.herokuapp.com")
@@ -193,7 +213,8 @@ describe("Api Put test  11", () => {
   });
 });
 
-describe("Api Put test  12", () => {
+
+describe("Api Put test  13", () => {
   it("should returns server error 404 since id is missing", done => {
     chai
       .request("https://ultimate-todo-web-postgres.herokuapp.com")
