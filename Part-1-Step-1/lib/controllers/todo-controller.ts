3ef36fb4ac7e9e38914277cfe6cd2324092b58e3
Syed-Kashan-Adil <syed.kashan.adil@gmail.com>
2018-09-27 23:48:01 +0500

    todo-controller.js comitted

--- Part-1-Step-1/lib/controllers/todo-controller.ts
+++ Part-1-Step-1/lib/controllers/todo-controller.ts
@@ -1,6 +1,7 @@
 import {todoSchema} from "../models/todo-model";
 import * as mongoose from "mongoose";
 import {Request,Response} from "express";
+import { error } from "util";
 
 const ToDo = mongoose.model("ToDo",todoSchema);
 
@@ -11,7 +12,14 @@ export class TodoController {
             status=true;
         else if(req.params.status === "false")
             status=false;
-            console.log(req.params.id);
+        else if(req.params.status === "null"){
+            res.status(404).send({message:"Invalid Done Status"})
+        }
+        else if(req.params.status === "undefined"){
+            res.status(404).send({message:"Invalid Done Status"})
+        }
+        else 
+            res.status(404).send({message:"Invalid Done Status"})   
         ToDo.findByIdAndUpdate(req.params.id,{$set:{Done:status}},{new:true}).then(data => {
             console.log(data);
             ToDo.findById({_id:data._id}).then(realData => {
@@ -41,12 +49,12 @@ export class TodoController {
         const task_id = req.params.id;
         ToDo.findById(task_id).then(data => {
             res.status(200).send(data);
-        })
+        }).catch(error => res.status(404).send({message:"Record Not Found"}))
     }
     public UpdateTask (req:Request,res:Response){
         const task_id=req.params.id;
         ToDo.findByIdAndUpdate(task_id,{$set:req.body}).then(data => {
-            ToDo.findById({_id:data._id}).then(realData => {
+            ToDo.findById(data._id).then(realData => {
                 res.status(200).send(realData)
             })
         })
@@ -63,7 +71,8 @@ export class TodoController {
                 }
             }
             res.send({status:false})
-        })
+        }).catch(error => res.status(404).send({status:false}))
+        
     }
     }
     
