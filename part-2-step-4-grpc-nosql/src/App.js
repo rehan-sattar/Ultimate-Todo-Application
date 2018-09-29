03086f9c61eb6ae691c85d221733eade10017368
Rehan-Sattar <you@example.com>
2018-09-29 20:55:57 +0500

    Ui upgraded

--- part-2-step-4-grpc-nosql/src/App.js
+++ part-2-step-4-grpc-nosql/src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import AppContainer from "./components/AppContainer";
+import "./App.css";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="py-3 bg-white text-center">
-          <h1>The Ultimate TodoList | grpc | nosql</h1>
+        <header id="header" className="text-center">
+          <i className="fas fa-clipboard-list mr-3"></i>The Ultimate TodoList
+          <br />
+          <small id="tagLine">Because management is everything.</small>
         </header>
         <AppContainer />
       </div>
     );
   }
 }
-
-
 
 export default App;
--- part-2-step-4-grpc-nosql/src/components/TodoList.jsx
+++ part-2-step-4-grpc-nosql/src/components/TodoList.jsx
@@ -19,11 +19,12 @@ class TodoList extends Component {
             updateDescription: '',
             updateDoneStatus: false,
 
+            addAction: false
+
         };
         this.handleTaskdone = this.handleTaskdone.bind(this);
         this.handleDeleteRequest = this.handleDeleteRequest.bind(this);
         this.handleUpdateRequest = this.handleUpdateRequest.bind(this);
-        this.onCloseModal = this.onCloseModal.bind(this);
     };
 
     onOpenModal = (id) => {
@@ -40,7 +41,8 @@ class TodoList extends Component {
     }
 
     componentWillReceiveProps(props) {
-        console.log('Component Will recieve Props: ', props);
+        console.log(props)
+        console.log(props);
         this.setState({
             todos: props.todos.todos
         });
@@ -63,8 +65,8 @@ class TodoList extends Component {
         this.onCloseModal();
     }
 
-    handleTaskdone(todo) {
-        this.props.changeDoneStatus(todo);
+    handleTaskdone(todo, status) {
+        this.props.changeDoneStatus(todo, status);
     }
     render() {
         const { open } = this.state;
@@ -72,50 +74,83 @@ class TodoList extends Component {
             <div>
                 <div className="container">
                     <div className="row justify-content-center">
-                        <div className="col-md-8 col-lg-8 col-sm-12">
-                            {this.state.todos ? this.state.todos.map((todo, index) => (
-                                <div key={index} className="card my-3">
-                                    <div className="card-body">
-                                        <h5>id: {index + 1}</h5>
-                                        <hr />
-                                        <h5>Title: {todo.title}</h5>
-                                        <hr />
-                                        <h5>Description: {todo.description}</h5>
-                                        <hr />
-                                        <h5>Status: {todo.doneStatus === true ? <span> Task completed! </span> : (<button onClick={() => this.handleTaskdone(todo)}>Done</button>)} </h5>
-                                    </div>
-                                    <div className="card-footer">
-                                        <button
-                                            className={"btn btn-success btn-lg mx-2"}
-                                            onClick={() => this.onOpenModal(todo.id)}>Update</button>
-                                        <button
-                                            className={"btn btn-danger btn-lg mx-2"}
-                                            onClick={() => this.handleDeleteRequest(todo.id)}>Delete</button>
-                                    </div>
-                                </div>
-                            )) : <p>No items</p>}
+                        <div className="col-md-10 col-lg-10 col-sm-12">
+                            {this.state.todos ? this.state.todos.map((todo, index) => {
+                                if (!todo.doneStatus) {
+                                    console.log("Done Status: ",todo.done)
+                                    return (
+                                        <div className="card my-5" key={index}>
+                                            <div className="card-body">
+                                                <h3 className="todotext">
+                                                    {todo.title}
+                                                </h3>
+                                                <p className="todotext">
+                                                    {todo.description}
+                                                </p>
+                                                <div className="btn-group float-right">
+                                                    <button
+                                                        onClick={() => this.handleTaskdone(todo.id, true)}
+                                                        className="btn btn-outline-success btn-lg card_btns"> <i className="fa fa-check"></i> </button>
+                                                    <button
+                                                        onClick={() => this.onOpenModal(todo.id)}
+                                                        className="btn btn-outline-warning btn-lg card_btns"> <i className="fas fa-pencil-alt"></i> </button>
+                                                    <button
+                                                        onClick={() => this.handleDeleteRequest(todo.id)}
+                                                        className="btn btn-outline-danger btn-lg card_btns"> <i className="fa fa-trash"></i> </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )
+                                } else {
+
+                                    console.log("Done Status: ",todo.done)
+                                    return (
+                                        <div className="card my-5 doneBody" key={index}>
+                                            <div className="card-body">
+                                                <h3 className="todotext crossed">
+                                                    {todo.title}
+                                                </h3>
+                                                <p className="todotext crossed">
+                                                    {todo.description}
+                                                </p>
+                                                <div className="btn-group float-right">
+                                                    <button
+                                                        onClick={() => this.handleTaskdone(todo.id, false)}
+                                                        className="btn btn-outline-warning btn-lg card_btns"> <i className="fas fa-undo"></i> </button>
+
+                                                    <button
+                                                        onClick={() => this.handleDeleteRequest(todo.id)}
+                                                        className="btn btn-outline-danger btn-lg card_btns"> <i className="fa fa-trash"></i> </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )
+                                }
+                            }) : <p>No items</p>}
                         </div>
-                    </div>
-                </div>
+                    </div >
+                </div >
 
                 <Modal open={open} onClose={this.onCloseModal} center>
-                    <h2>Update Todo</h2>
+                    <h2 className="text-center my-2 pt-2">Update Todo</h2>
                     <form onSubmit={this.handleUpdateRequest}>
                         <input
                             type="text"
                             placeholder="Title"
                             required
+                            className="form-control"
                             onChange={(e) => this.setState({ updateTitle: e.target.value })} />
                         <br />
                         <br />
                         <textarea
                             placeholder="Description"
                             required
+                            className="form-control"
                             onChange={(e) => this.setState({ updateDescription: e.target.value })} >
                         </textarea>
                         <br />
                         <br />
-                        <button type={"Submit"}>Add Task</button>
+                        <button className="btn btn-primary btn-block" type={"Submit"}>Update</button>
                     </form>
                 </Modal>
             </div >
@@ -123,14 +158,17 @@ class TodoList extends Component {
     };
 };
 
-const mapStateToProps = (state) => ({
-    todos: state.allTodos
-})
+const mapStateToProps = (state) => {
+    return {
+        todos: state.allTodos,
+        addedTodoAction: state.addedTodo
+    }
+}
 const mapDispatchToProps = (dispatch) => bindActionCreators({
     downloadTodos: (Todostate) => getAllTodosFromDatabase(Todostate),
     deleteATask: (todoId) => deleterTodoFromDatabase(todoId),
     updateTodoTask: (newTodoObject) => updateTodoInDatabase(newTodoObject),
-    changeDoneStatus: (todo) => taskDoneAttempt(todo)
+    changeDoneStatus: (todo, status) => taskDoneAttempt(todo,status )
 },
     dispatch);
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
